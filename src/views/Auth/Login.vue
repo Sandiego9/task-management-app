9051078187eae8213eba431d87921e9e91f00c21
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2025-07-15 01:24:36 +0100

    feat: template Login, register, dashboard, and profile views

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="p-4">
+    <h2 class="text-xl font-bold mb-4">Login Page</h2>
+    <Button label="Click Me" @click="sayHello" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button"
+
+function sayHello() {
+  alert('Vue + TS + PrimeVue setup is working!');
+}
+</script>
---
+++ src/views/Dashboard/TaskDashboard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="p-4">
+    <h1>TaskDashboard Page</h1>
+  </div>
+</template>
