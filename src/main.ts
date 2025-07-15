7ba113dc0c497106cdefdf79dad8a1c788e6b0c8
Henry Tochukwu Uzor <h.uzor17@gmail.com>
2025-07-15 01:22:08 +0100

    feat: main.ts file setup with required plugins

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,22 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
-createApp(App).mount('#app')
+
+import router from "./router";
+import { createPinia } from "pinia";
+
+const pinia = createPinia();
+const app = createApp(App);
+app.use(router);
+app.use(pinia);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
+app.mount('#app');
