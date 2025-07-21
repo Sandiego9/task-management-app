b4b24f77afbb167ba300a585b360b10cf26d378d
Henry Tochukwu Uzor <henry.uzor@cyphercrescent.com>
2025-07-21 13:25:09 +0100

    feat: dashboard route protection

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import { auth } from "../utils/firebase";
 
 const routes = [
   {
-    path: '/',
-    redirect: '/login'
+    path: "/",
+    redirect: "/login",
   },
   {
-    path: '/login',
-    component: () => import('@/views/Auth/Login.vue')
+    path: "/login",
+    component: () => import("@/views/Auth/Login.vue"),
   },
   {
-    path: '/register',
-    component: () => import('@/views/Auth/Register.vue')
+    path: "/register",
+    component: () => import("@/views/Auth/Register.vue"),
   },
   {
-    path: '/forgot-password',
-    component: () => import('@/views/Auth/ForgotPassword.vue')
+    path: "/forgot-password",
+    component: () => import("@/views/Auth/ForgotPassword.vue"),
   },
   {
-    path: '/dashboard',
-    component: () => import('@/views/Dashboard/TaskDashboard.vue')
+    path: "/dashboard",
+    component: () => import("@/views/Dashboard/TaskDashboard.vue"),
+    meta: { requiresAuth: true },
   },
   {
-    path: '/profile',
-    component: () => import('@/views/Profile/ProfileView.vue')
+    path: "/profile",
+    component: () => import("@/views/Profile/ProfileView.vue"),
+    meta: { requiresAuth: true },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const currentUser = auth.currentUser;
+  const requiresAuth = to.meta.requiresAuth;
+
+  if (requiresAuth && !currentUser) {
+    next("/login");
+  } else if (to.path === "/login" && currentUser) {
+    next("/dashboard");
+  } else {
+    next();
+  }
 });
 
 export default router;
